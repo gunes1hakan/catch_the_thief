571b2f42b642ffaacb032ac7eab5f1469dd17121
gunes1hakan <gunes1.hakan1@gmail.com>
2026-01-22 01:57:50 +0300

    Completed the groundwork for the next steps.
    --Dynamically populated the imageViews array by iterating through the GridLayout children, and assigned the onClick listeners within this loop to eliminate manual repetition and optimize the code structure.
    --Set the visibility of the ImageViews to invisible.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.myapplication;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import androidx.gridlayout.widget.GridLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     TextView textViewTime;
     TextView textViewScore;
+    ImageView[] imageViews;
     int score;
 
     @Override
@@ -21,7 +24,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         score=0;
+
+        GridLayout grid=findViewById(R.id.gridLayout);      //Stored the ImageViews located within the GridLayout into an array
+        int n=grid.getChildCount();
+        imageViews=new ImageView[n];
+        for(int i=0; i<n; i++){
+            View child = grid.getChildAt(i);
+            imageViews[i] = (ImageView) child;
+            imageViews[i].setOnClickListener(this::increaseScore);
+        }
+        hideImages();
+
         new CountDownTimer(10000,1000){     //Implemented the countdown timer logic
 
             @Override
@@ -33,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
             public void onTick(long millisUntilFinished) {
                 textViewTime.setText("Time: "+millisUntilFinished/1000);
             }
-        };
+        }.start();
         textViewTime=findViewById(R.id.textViewTime);
         textViewScore=findViewById(R.id.textViewScore);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -46,6 +61,12 @@ public class MainActivity extends AppCompatActivity {
     public void increaseScore(View view){       //onclick method of all imageViews
         score++;
         textViewScore.setText("Score: "+score);
+    }
+
+    public void hideImages(){       //Set the visibility of the ImageViews to invisible
+        for(ImageView i:imageViews){
+            i.setVisibility(View.INVISIBLE);
+        }
     }
 
 }
