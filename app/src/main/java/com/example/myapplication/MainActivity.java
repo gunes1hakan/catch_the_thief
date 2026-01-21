f4addfa31cbdd34466d2fbdf56393c538ad63c63
gunes1hakan <gunes1.hakan1@gmail.com>
2026-01-22 01:13:54 +0300

    Implemented score increase upon clicking the image.
    --Assigned a shared onClick method named increaseScore to every ImageView.
    --Defined the increaseScore method in MainActivity and implemented the score update logic.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textViewTime;
+    TextView textViewScore;
+    int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        score=0;
+        textViewTime=findViewById(R.id.textViewTime);
+        textViewScore=findViewById(R.id.textViewScore);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void increaseScore(View view){       //onclick method of all imageViews
+        score++;
+        textViewScore.setText("Score: "+score);
     }
 }
